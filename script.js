b12ac528b3576811dc816784e66feb4390002a5b
IanCist <iantcisternas@gmail.com>
2025-05-29 09:26:14 -0400

    added contact page

--- script.js
+++ script.js
@@ -35,12 +35,25 @@ if (typingEl) {
 window.addEventListener('load', () => {
     const loader = document.querySelector('.loader-wrapper');
     setTimeout(() => {
-      loader.style.transition = 'opacity 0.6s ease';
-      loader.style.opacity = '0';
-      setTimeout(() => {
-          loader.style.display = 'none';
-      }, 600);
-    }, 2000); 
+        loader.style.transition = 'opacity 0.6s ease';
+        loader.style.opacity = '0';
+        setTimeout(() => {
+            loader.style.display = 'none';
+        }, 600);
+    }, 2000);
 });
 
 
+(() => {
+    'use strict';
+    const forms = document.querySelectorAll('.contact-form');
+    Array.from(forms).forEach(form => {
+        form.addEventListener('submit', event => {
+            if (!form.checkValidity()) {
+                event.preventDefault();
+                event.stopPropagation();
+            }
+            form.classList.add('was-validated');
+        }, false);
+    });
+})();
